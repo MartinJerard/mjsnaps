0c2e516aeb510abb7fce07346371969b364b8d17
MartinJerard <95540694+MartinJerard@users.noreply.github.com>
2022-12-21 16:51:52 +0530

    Update BlogPosts.js
    Editing post detail with break statements

--- src/pages/BlogPosts.js
+++ src/pages/BlogPosts.js
@@ -73,6 +73,8 @@ export default () => {
       case 'heading-three':
         return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
       case 'paragraph':
+        if modifiedText === ""
+          return <br>;
         return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
       case 'heading-four':
         return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
